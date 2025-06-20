db289c7e60cd04c2ec8620300af52df709c10e1d
RenatoFerreira1240605 <1240605@isep.ipp.pt>
2025-06-20 10:21:27 +0100

    fixed builder configurations

--- SIAVIBioFITBackEnd/Program.cs
+++ SIAVIBioFITBackEnd/Program.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using SiaviBioFit.Shared.Services;
 using SIAVIBioFITBackEnd;
 using SIAVIBioFITBackEnd.Data;
@@ -28,18 +29,41 @@ namespace SIAVIBioFITBackEnd
             builder.Services.AddScoped<ExerciseService>();
             builder.Services.AddScoped<ExerciseLogService>();
 
-            builder.WebHost.ConfigureKestrel(serverOptions =>
+            // Configura CORS (permite chamadas de qualquer origem)
+            builder.Services.AddCors(options =>
             {
-                serverOptions.ListenAnyIP(PortFromEnv());
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
             });
 
-            int PortFromEnv()
+            // Adiciona controladores
+            builder.Services.AddControllers();
+
+            // Ativa Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
             {
-                var port = Environment.GetEnvironmentVariable("PORT");
-                return string.IsNullOrEmpty(port) ? 5000 : int.Parse(port);
-            }
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Isep API", Version = "v1" });
+
+                // Define servidor base do Swagger
+#if DEBUG
+                c.AddServer(new OpenApiServer { Url = "http://localhost:8080" });
+#else
+                c.AddServer(new OpenApiServer { Url = "https://siavibiofit-backend.onrender.com/" });
+#endif
+
+            });
 
             var app = builder.Build();
+
+            // Força a API a ouvir em http://localhost:8080
+            //app.Urls.Add("http://localhost:8080");
+            //app.Urls.Add("http://0.0.0.0:8080");
+
 
             // Configure the HTTP request pipeline.
 
